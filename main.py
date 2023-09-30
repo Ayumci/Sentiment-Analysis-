db044d40eea4799ac8c7ea17b1640e6769fb4c1c
Ayumci <2028daniel01@stu.sbs.ac.th>
2023-09-30 10:24:41 +0700

    Update code

--- main.py
+++ main.py
@@ -53,6 +53,7 @@ model.add(Dense(2, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 plot_model(model, show_shapes=True)
-epoes = 10
+#Epochs and batch sizes
+epo = 10
 batch = 512
-history = model.fit(X_train_pad, y_train, epochs=epoes, batch_size=batch, validation_split=0.2, )
+history = model.fit(X_train_pad, y_train, epochs=epo, batch_size=batch, validation_split=0.2, )
